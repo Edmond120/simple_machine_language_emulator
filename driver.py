186d98bb86429cb7dd4cf06c16e830707a4c7d90
edmond120 <edmondwong888@msn.com>
2019-10-13 21:01:24 -0400

    changed how settings are applied and added printing function

--- driver.py
+++ driver.py
@@ -4,8 +4,12 @@ def main():
 	memory_file_path   = 'memory'
 	register_file_path = 'registers'
 	start_address      = '0x0'
+
+	step = False
+	print_init = True
+
 	with open(memory_file_path,'r') as memory_file, open(register_file_path,'r') as register_file:
-		emulator.emulate(memory_file,register_file,start_address)
+		emulator.emulate(memory_file,register_file,start_address,{'step':step,'print_init':print_init})
 
 if __name__ == '__main__':
 	main()
--- emulator.py
+++ emulator.py
@@ -1,13 +1,16 @@
-def emulate(memory_file,register_file,start_address,step=False):
+def emulate(memory_file,register_file,start_address,settings):
 	memory    = load_data(memory_file)
 	registers = load_data(register_file)
 	program_counter = int(start_address,16)
 	instruction_register = memory[program_counter] * 16 + memory[program_counter + 1]
-	settings = {'step':step}
 	run_emulator(memory,registers,program_counter,instruction_register,settings)
 
 def run_emulator(memory,registers,program_counter,instruction_register,settings):
-	pass
+	if settings['print_init']:
+		print("memory:")
+		print_data(memory)
+		print("registers:")
+		print_data(registers)
 
 def load_data(data_file):
 	data={}
@@ -15,5 +18,8 @@ def load_data(data_file):
 		if len(line) == 0 or line[0] == '#':
 			continue
 		address, info = line.split(" ")
-		data[int(address,16)] = int(data,16)
+		data[int(address,16)] = int(info,16)
 	return data
+
+def print_data(data):
+	print(list(map(lambda x: (hex(x),hex(data[x]),),sorted(data.keys()))))
