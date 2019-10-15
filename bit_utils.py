be993f995ffc2e6b8a56b0b5b5bef50a22a23889
edmond120 <edmondwong888@msn.com>
2019-10-15 19:52:31 -0400

    added new incomplete functions in bit_utils

--- bit_utils.py
+++ bit_utils.py
@@ -1,3 +1,17 @@
 def overflow(integer, bits): #incomplete
 	"""returns <integer>, truncating it if it exceeds <bits> bits"""
 	return integer
+
+def us_to_tc(unsigned_int, settings): #incomplete
+	"""
+	returns the number unsigned_int would be if it was in two's complement
+	notation
+	"""
+	return 0
+
+def tc_to_us(twos_complement_int, settings): #incomplete
+	"""
+	returns the number twos_complement_int would be if it was treated as an
+	unsigned int notation
+	"""
+	return 0
