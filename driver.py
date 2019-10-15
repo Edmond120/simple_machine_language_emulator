8dd4cd1b48dd1ee51137a5842e568d2ea94b0caa
edmond120 <edmondwong888@msn.com>
2019-10-15 12:45:52 -0400

    readability changes in driver.py

--- driver.py
+++ driver.py
@@ -7,9 +7,16 @@ def main():
 
 	step = False
 	print_init = True
+	memory_size_bits = 8
+
+	settings = {
+				'step'            : False,
+				'print_init'      : True,
+				'memory_size_bits': 8,
+	}
 
 	with open(memory_file_path,'r') as memory_file, open(register_file_path,'r') as register_file:
-		emulator.emulate(memory_file,register_file,start_address,{'step':step,'print_init':print_init})
+		emulator.emulate(memory_file,register_file,start_address,settings)
 
 if __name__ == '__main__':
 	main()
