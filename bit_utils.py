aa2414d5d2523494374c4cebdcbc614c4e81f073
edmond120 <edmondwong888@msn.com>
2019-10-15 20:51:22 -0400

    finished two functions

--- bit_utils.py
+++ bit_utils.py
@@ -2,16 +2,24 @@ def overflow(integer, bits): #incomplete
 	"""returns <integer>, truncating it if it exceeds <bits> bits"""
 	return integer
 
-def us_to_tc(unsigned_int, bits): #incomplete
+def us_to_tc(unsigned_int, bits):
 	"""
 	returns the number unsigned_int would be if it was in two's complement
 	notation
 	"""
-	return 0
+	if (2 ** (bits - 1)) <= unsigned_int:
+		#negative
+		return unsigned_int - 2**bits
+	else:
+		#positive
+		return unsigned_int
 
-def tc_to_us(twos_complement_int, bits): #incomplete
+def tc_to_us(twos_complement_int, bits):
 	"""
 	returns the number twos_complement_int would be if it was treated as an
 	unsigned int notation
 	"""
-	return 0
+	if twos_complement_int >= 0:
+		return twos_complement_int
+	else:
+		return twos_complement_int + 2**bits
