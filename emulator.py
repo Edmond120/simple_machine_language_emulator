a385ddfcd637981612ef479693a3e757b7853bdc
edmond120 <edmondwong888@msn.com>
2019-10-29 14:47:43 -0400

    changed how memory is printed

--- emulator.py
+++ emulator.py
@@ -1,7 +1,7 @@
 import simple_machine_language
 
 def emulate(memory_file,register_file,start_address,settings):
-	memory    = load_data(memory_file)
+	memory	= load_data(memory_file)
 	registers = load_data(register_file)
 	program_counter = int(start_address,16)
 	instruction_register = memory[program_counter] * 16 + memory[program_counter + 1]
@@ -9,9 +9,11 @@ def emulate(memory_file,register_file,start_address,settings):
 
 def run_emulator(memory,registers,program_counter,instruction_register,settings):
 	if settings['print_init']:
-		print("memory:")
+		print('program_counter: ' + hex(program_counter))
+		print('instruction_register: ' + hex(instruction_register))
+		print('memory:')
 		print_data(memory)
-		print("registers:")
+		print('registers:')
 		print_data(registers)
 
 def load_data(data_file):
@@ -26,5 +28,7 @@ def load_data(data_file):
 def _data_list(data):
 	return list(map(lambda x: (hex(x),hex(data[x]),),sorted(data.keys())))
 
-def print_data(data):
-	print(_data_list(data))
+def print_data(data, head='\t', spacing=' : ', tail='\n'):
+	dl = _data_list(data)
+	for item in dl:
+		print(head + item[0] + spacing + item[1], end=tail)
